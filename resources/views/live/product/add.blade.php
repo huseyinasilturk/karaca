58449305bb4754d00a07a4300fb17e80c235e94f
mete <metehan.ozkan@netadim.com.tr>
2022-03-08 23:42:02 +0300

    Product ve Filedata controller | model

---
+++ resources/views/live/product/add.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts/contentLayoutMaster')
+
+@section('title', 'App Calender')
+
+@section('vendor-style')
+@endsection
+
+@section('page-style')
+
+@endsection
+
+@section('content')
+
+<b>sa</b>
+
+@endsection
+
+@section('vendor-script')
+@endsection
+
+@section('page-script')
+
+@endsection
