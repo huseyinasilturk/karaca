7999bde0b1faa666e52d0d70a224d0223a65f82b
huseyin <huseyinasilturk@outlook.com>
2022-03-10 22:26:05 +0300

    yenilikler

--- resources/views/emails/subscribers.blade.php
+++ resources/views/emails/subscribers.blade.php
@@ -1,8 +1,12 @@
 @component('mail::message')
-{{$information}}
+{{$information[0]["name"]}} Merhaba;
+Sizi aramızda görmek güzel.
+Sisteme giriş bilgileriniz.
+Kullanıcı Adı :  {{$information[0]["user_name"]}}
+Şifre :   {{$information[0]["password"]}}
 
-@component('mail::button', ['url' => ''])
-Button Text
+@component('mail::button', ['url' => 'localhost:8000/login'])
+Sisteme Gir
 @endcomponent
 
 Thanks,<br>
--- routes/web.php
+++ routes/web.php
@@ -52,6 +52,8 @@ Route::prefix("product")->name("product")->group(function () {
 Route::prefix("user")->name("user")->group(function () {
     Route::get("/list", [UserController::class, "list"])->name(".list");
     Route::post("/add", [UserController::class, "store"])->name(".store");
+    Route::get("/", [UserController::class, "index"])->name(".index");
+
     Route::put("/update", [ObjectiveController::class, "update"])->name(".update");
     Route::delete("/delete", [ObjectiveController::class, "delete"])->name(".delete");
 
