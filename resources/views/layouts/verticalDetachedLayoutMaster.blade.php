06e78131ac950c450734cdae766ab698a1499e73
Wikuu <a4m4ttjob@gmail.com>
2022-03-16 00:46:06 +0300

    Stok ekleme ve stok filtreleme tamamlandı

--- resources/views/layouts/verticalDetachedLayoutMaster.blade.php
+++ resources/views/layouts/verticalDetachedLayoutMaster.blade.php
@@ -59,6 +59,12 @@
                 })
             }
         })
+
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
     </script>
 </body>
 
