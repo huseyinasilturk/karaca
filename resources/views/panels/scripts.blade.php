bbd6b889f54f2ff26e25b9a8992cddc67422a4c2
Wikuu <a4m4ttjob@gmail.com>
2022-03-27 10:19:57 +0300

    Iconların görünmeme sorunu düzeltildi

--- resources/views/panels/scripts.blade.php
+++ resources/views/panels/scripts.blade.php
@@ -1,10 +1,12 @@
 <!-- BEGIN: Vendor JS-->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.26.1/axios.min.js" integrity="sha512-bPh3uwgU5qEMipS/VOmRqynnMXGGSRv+72H/N260MQeXZIK4PG48401Bsby9Nq5P5fz7hy5UGNmC/W1Z51h2GQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.26.1/axios.min.js"
+integrity="sha512-bPh3uwgU5qEMipS/VOmRqynnMXGGSRv+72H/N260MQeXZIK4PG48401Bsby9Nq5P5fz7hy5UGNmC/W1Z51h2GQ=="
+crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 <script src="{{ asset(mix('vendors/js/vendors.min.js')) }}"></script>
 <!-- BEGIN Vendor JS-->
 <!-- BEGIN: Page Vendor JS-->
-<script src="{{asset(mix('vendors/js/ui/jquery.sticky.js'))}}"></script>
+<script src="{{ asset(mix('vendors/js/ui/jquery.sticky.js')) }}"></script>
 @yield('vendor-script')
 <!-- END: Page Vendor JS-->
 <!-- BEGIN: Theme JS-->
@@ -14,12 +16,10 @@
 <!-- custome scripts file for user -->
 <script src="{{ asset(mix('js/core/scripts.js')) }}"></script>
 
-@if($configData['blankPage'] === false)
-<script src="{{ asset(mix('js/scripts/customizer.js')) }}"></script>
+@if ($configData['blankPage'] === false)
+    <script src="{{ asset(mix('js/scripts/customizer.js')) }}"></script>
 @endif
 <!-- END: Theme JS-->
 <!-- BEGIN: Page JS-->
 @yield('page-script')
 <!-- END: Page JS-->
-
-
