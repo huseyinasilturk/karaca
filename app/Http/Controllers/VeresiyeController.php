01c2915ab18ac7fc427ba35b197a41d9fb394096
huseyin <huseyinasilturk@outlook.com>
2022-07-18 22:45:12 +0300

    deneme

--- app/Http/Controllers/VeresiyeController.php
+++ app/Http/Controllers/VeresiyeController.php
@@ -20,7 +20,7 @@ class VeresiyeController extends Controller
         // SELECT * FROM expense_statements e INNER JOIN objectives o ON e.expense_type_id = o.id
         // SELECT * FROM expense_statements AS es JOIN objectives o ON es.expense_type_id = o.id
 
-        $ExpenseObjective = DB::select("SELECT e.id as aid , e.* , o.* FROM expense_statements AS e LEFT JOIN objectives AS o ON e.expense_type_id = o.id");
+        $ExpenseObjective = DB::select("SELECT e.id as aid , e.* , o.* FROM expense_statements AS e LEFT JOIN objectives AS o ON e.expense_type_id = o.id where  1 = 2 " );
         $musteriler = Customer::all();
         $totalSum = DB::select("SELECT MONTH(expense_date) AS mouth, YEAR(expense_date) AS YEAR, SUM(expense_statements.price) AS totalSum
         FROM expense_statements
@@ -28,7 +28,7 @@ class VeresiyeController extends Controller
 
         $totalSumAy = DB::select("SELECT MONTH(expense_date) AS mouth, YEAR(expense_date) AS YEAR, SUM(expense_statements.price) AS totalSum
         FROM expense_statements
-        WHERE YEAR(expense_date) = YEAR(DATE(NOW())) AND MONTH(expense_date) = MONTH(DATE(NOW()))  AND company_id =  " . auth()->user()->company_id);
+        WHERE YEAR(expense_date) = YEAR(DATE(NOW())) AND 1 = 2 and  MONTH(expense_date) = MONTH(DATE(NOW()))  AND company_id =  " . auth()->user()->company_id);
 
         return view("live.veresiye.index", compact("ExpenseObjective", "totalSum", "totalSumAy", "ExpenseType","musteriler"));
     }
